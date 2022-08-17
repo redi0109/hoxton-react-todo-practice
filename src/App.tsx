24ab2f8e8f05c4fe4ce2706a99101afa64348966
Demir Boraj <demirboraj97@gmail.com>
2022-08-17 23:30:24 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,17 @@ function App() {
       .then((todosFromServer) => setTodos(todosFromServer));
   }, []);
 
+  function searchTodo (){
+    let filteredTodos = todos.filter(todo => todo.title?.toLocaleLowerCase().includes(content?.toLocaleLowerCase()));
+    return filteredTodos;
+  }
+
+ function toggleTodo(item:Todo){
+  const newTodos = structuredClone(todos);
+  const index = newTodos.findIndex((todo: { id: number; }) => todo.id === item.id);
+  newTodos[index].completed = !newTodos[index].completed;
+  setTodos(newTodos);
+ }
 
 
 
@@ -26,16 +37,28 @@ function App() {
       <header className="App-header">
         <h1>Todo List</h1>
         <input type="text" placeholder="Search Todo"
-          onChange={(event)=>{
-            setContent(event.target.value)
-            searchTodo()
-          }} />
+        onChange={(event) => { setContent(event.target.value)
+        searchTodo()}}/>
       </header>
       <main className="App-main">
         <ul className="todo-list">
-          {todos.map((todo) => (
-            <li key={todo.id} className={todo.completed ? "completed" : "not-completed"}>
-              {todo.title}
+          {searchTodo().map((item) => (
+            <li key={item.id} className={item.completed ? "completed" : "not-completed"}
+            onClick={()=>{
+              toggleTodo(item)
+              fetch(`http://localhost:3333/todos/${item.id}`,{
+                method:'PATCH',
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  completed: !item.completed
+                })
+              })
+            }
+            }
+            >
+              {item.title}
             </li>
           ))}
            
@@ -43,8 +66,29 @@ function App() {
         
         </ul>
 
-        <form>
-          <input type="text" placeholder="Add Todo" />
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            let newTodo = {
+              content: event.target.newTodo.value,
+              completed: false,
+            };
+            setTodos([...todos, newTodo]);
+            fetch("http://localhost:4000/todos", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                content: event.target.newTodo.value,
+                completed: false,
+              }),
+            });
+            event.target.reset();
+          }}
+        >
+          <input placeholder="add a todo" name="newTodo"></input>
+          <button>Submit</button>
         </form>
       </main>
     </div>
